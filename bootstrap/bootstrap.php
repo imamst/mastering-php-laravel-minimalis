47d9511d4b000629e9f4b5b1b696eeb4bb379a24
Imam Setiawan <s.imam29@gmail.com>
2023-10-15 07:58:50 +0700

    init setup

---
+++ bootstrap/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__.'../vendor/autoload.php';
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', realpath(__DIR__.DS.'..'));
+define('CONFIG_ROOT_PATH', realpath(__DIR__.DS.'..'.DS.'config'));
